14169fc66a9da51e4eb0802bc0901ad9325474fb
alicia_s_souza@estudante.sesisenai.org.br <alicia_s_souza@estudante.sesisenai.org.br>
2026-08-25 15:32:35 -0300

    feito

--- script.js
+++ script.js
@@ -40,7 +40,12 @@ btnVisita.addEventListener('click', function() {
 //    - Use um for ou forEach para percorrer cada um
 //    - Leia o valor: elemento.getAttribute('data-valor')
 //    - Aplique a largura: elemento.style.width = valor + '%'
-//
+//const barras = document.querySelectorAll('.progresso');
+
+barras.forEach(function(elemento) {
+    let valor = elemento.getAttribute('data-valor');
+    elemento.style.width = valor + '%';
+});
 // 4. FORMULARIO (#form-contato)
 //    - Pegue o formulario com getElementById()
 //    - Adicione addEventListener('submit', function(evento) { })
